9bbff315b7f7f347f2a5227a8b64d76dce61ddf3
Patrick Pluequett <ppluequett@gmail.com>
2022-03-28 16:46:52 +0200

    add get route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,22 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+app.use(express.json());
+
+app.use(cors());
+
 app.get('/test', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/filmitems', (req, res) => {
+  res.send('FILMITEMS');
+});
+
+/*
+
+route get filmitems || select * from filmitem
+route get user || login
+route post user || registrieren
+
+*/
